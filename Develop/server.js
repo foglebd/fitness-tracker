9c01ddeb924d828080239f13713a9c9de00b6cea
Blake Fogle <foglebd@gmail.com>
2020-02-08 16:59:21 -0800

    routes added to server file

--- Develop/server.js
+++ Develop/server.js
@@ -22,6 +22,18 @@ mongoose.connect("mongodb://localhost/workout", {
 app.use(require("./routes/api.js"));
 app.use(require("./routes/view.js"));
 
+app.get ("/", function (req, res){
+  res.sendFile(path.join(_dirname, "./public/index.html"))
+});
+
+app.get("/exercise", function (req, res) {
+  res.sendFile(path.join(__dirname, "./public/exercise.html"));
+});
+
+app.get("/stats", function (req, res) {
+  res.sendFile(path.join(__dirname, "./public/stats.html"));
+});
+
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
